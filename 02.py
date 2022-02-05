248e0e07a12e8c9afd1165501114065ec3218f81
yukiya119 <yukinosuke911@gmail.com>
2022-02-05 16:23:09 +0900

    おみくじアプリを「omikuji.html」に組込み

--- 02.py
+++ 02.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,6 +13,14 @@ def index():
 @app.route("/user/<name>")
 def hello(name):
     return render_template("hello.html", name=name)
+
+
+# おみくじアプリ
+@app.route("/omikuji")
+def omikuji():
+    result = random.choice(["大吉!!", "中吉", "小吉", "吉", "凶"])
+
+    return render_template("omikuji.html", result=result)
 
 
 if __name__ == '__main__':
