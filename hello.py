93b6792b54be0574e19c122d8852f49a668fd4d2
yukiya119 <yukinosuke911@gmail.com>
2022-02-05 13:59:14 +0900

    login!!を表示させるコードを追加

--- hello.py
+++ hello.py
@@ -8,6 +8,12 @@ def hello_world():
     return "Hello,World"
 
 
+# ログイン機能
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return "login!!"
+
+
 if __name__ == '__main__':
     hostname = "127.0.0.1"
     port = 5001
